1461e6c22d9037bb1af4ac1268f0b3004963cd30
RomanDubinin <Roman230394@yandex.ru>
2016-04-08 13:00:20 +0500

    число k

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from src.full_search import get_all_partitions
 from itertools import permutations
 from math import sqrt
 from operator import sub
+import math
 
 from src.graph_visualize import visualize, experimental_antibug_visualize
 
@@ -37,61 +38,79 @@ def get_optimal_salesman_tour(depos, set_of_customers):
 
         return (optimal_depo, optimal_tour)
 
-depos = [
-         (5,5,0),
-    (0,0,0),
-         (0,5,0),
-         (4,0,0)
-         ]
+def get_optimal_cvrp_solution(depos, customers, q):
+    partitions = get_all_partitions(set(customers), q)
+    optimal_solution = []
+    min_solution_len = float("inf")
 
-customers = [(1,0,0),
-             (2,1.5,0),
-             (2,0.5,0),
-             (2,3,0),
-             (0,4,0),
-             (5,4,0),
-             (4,4,0)]
+    for partition in partitions:
+        solution = []
 
-q = 2
+        for subset in partition:
+            optimal_depo, optimal_tour = get_optimal_salesman_tour(depos, subset)
+            solution.append((optimal_depo, optimal_tour))
+
+        solution_len = sum([get_tour_len(elem[0], elem[1]) for elem in solution])
+
+        if solution_len < min_solution_len:
+            min_solution_len = solution_len
+            optimal_solution = solution
+
+    return optimal_solution
+
+def count_k(depos_count, capacity, epsilon):
+    c1 = 10 + 4*math.pi
+    c2 = 8 * sqrt(math.pi)
+    return math.ceil((1 + (depos_count*c1)/(2*(capacity-1))) *
+                     (math.exp(capacity*(capacity-1)/epsilon + sqrt(2*c2*c2/c1) * sqrt(depos_count*capacity/epsilon)) - 1))
+
+def main():
+
+    depos = [
+             (5,5,0),
+             (0,0,0),
+             (0,5,0),
+             (4,0,0)
+             ]
+
+    customers = [(1,0,0),
+                 (2,1.5,0),
+                 (2,0.5,0),
+                 (2,3,0),
+                 (0,4,0),
+                 (5,4,0),
+                 (4,4,0)]
+
+    q = 2
 
 
-partitions = get_all_partitions(set(customers), q)
-optimal_solution = []
-min_solution_len = float("inf")
+    optimal_solution = get_optimal_cvrp_solution(depos, customers, q)
 
-for partition in partitions:
-    solution = []
+    for tour in optimal_solution:
+        print(tour)
 
-    for subset in partition:
-        optimal_depo, optimal_tour = get_optimal_salesman_tour(depos, subset)
-        solution.append((optimal_depo, optimal_tour))
-
-    solution_len = sum([get_tour_len(elem[0], elem[1]) for elem in solution])
-
-    if solution_len < min_solution_len:
-        min_solution_len = solution_len
-        optimal_solution = solution
-
-for tour in optimal_solution:
-    print(tour)
-
-#######################################
+    #######################################
 
 
-customer_nodes = []
-for tour in optimal_solution:
-    for customer in tour[1]:
-        customer_nodes.append({"name": str(customer), "group": depos.index(tour[0]), "coords": customer})
+    customer_nodes = []
+    for tour in optimal_solution:
+        for customer in tour[1]:
+            customer_nodes.append({"name": str(customer), "group": depos.index(tour[0]), "coords": customer})
 
-depo_nodes = []
-for depo in depos:
-    depo_nodes.append({"name": str(depo), "group": depos.index(depo), "coords": depo})
+    depo_nodes = []
+    for depo in depos:
+        depo_nodes.append({"name": str(depo), "group": depos.index(depo), "coords": depo})
 
-edges = []
-for tour in optimal_solution:
-    edges.append({"source":tour[0], "target": tour[1][0]})
-    edges.append({"source":tour[1][-1], "target": tour[0]})
-    for i in range(len(tour[1]) - 1):
-        edges.append({"source": tour[1][i], "target": tour[1][i+1]})
+    edges = []
+    for tour in optimal_solution:
+        edges.append({"source":tour[0], "target": tour[1][0]})
+        edges.append({"source":tour[1][-1], "target": tour[0]})
+        for i in range(len(tour[1]) - 1):
+            edges.append({"source": tour[1][i], "target": tour[1][i+1]})
 
-experimental_antibug_visualize(depo_nodes, customer_nodes, edges)
+    experimental_antibug_visualize(depo_nodes, customer_nodes, edges)
+
+
+main()
+
+print(count_k(5, 10, 10))
